8047af6333be6e4efa4369f9b3cfa00a3400cba3
Yun-Tse Tsai <yuntsebaryon@gmail.com>
2022-12-08 17:54:56 -0800

    Code without ethernet

---
+++ ArduinoCode/GasHeaterControlNoEthernet/GasHeaterControlNoEthernet.ino
@@ -0,0 +1,73 @@
+// Basic example using one-shot measurement.
+// The call to readThermocoupleTemperature() is blocking for O(100ms)
+
+#include <Adafruit_MAX31856.h>
+
+// Use software SPI: CS, DI, DO, CLK
+Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(13, 12, 11, 10);
+// use hardware SPI, just pass in the CS pin
+//Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(10);
+
+// Relay for the heater power circuit
+const int heater = 8;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+  Serial.println("MAX31856 thermocouple test");
+
+  maxthermo.begin();
+
+  maxthermo.setThermocoupleType(MAX31856_TCTYPE_K);
+
+  Serial.print("Thermocouple type: ");
+  switch (maxthermo.getThermocoupleType() ) {
+    case MAX31856_TCTYPE_B: Serial.println("B Type"); break;
+    case MAX31856_TCTYPE_E: Serial.println("E Type"); break;
+    case MAX31856_TCTYPE_J: Serial.println("J Type"); break;
+    case MAX31856_TCTYPE_K: Serial.println("K Type"); break;
+    case MAX31856_TCTYPE_N: Serial.println("N Type"); break;
+    case MAX31856_TCTYPE_R: Serial.println("R Type"); break;
+    case MAX31856_TCTYPE_S: Serial.println("S Type"); break;
+    case MAX31856_TCTYPE_T: Serial.println("T Type"); break;
+    case MAX31856_VMODE_G8: Serial.println("Voltage x8 Gain mode"); break;
+    case MAX31856_VMODE_G32: Serial.println("Voltage x8 Gain mode"); break;
+    default: Serial.println("Unknown"); break;
+  }
+
+  pinMode(heater, OUTPUT);
+
+}
+
+void loop() {
+
+  // Read the thermocouple
+  Serial.print("Cold Junction Temp: ");
+  float CJTTemp = maxthermo.readCJTemperature();
+  Serial.println(CJTTemp);
+
+  Serial.print("Thermocouple Temp: ");
+  float TCTemp = maxthermo.readThermocoupleTemperature();
+  Serial.println(TCTemp);
+  
+  // Check and print any faults
+  uint8_t fault = maxthermo.readFault();
+  if (fault) {
+    if (fault & MAX31856_FAULT_CJRANGE) Serial.println("Cold Junction Range Fault");
+    if (fault & MAX31856_FAULT_TCRANGE) Serial.println("Thermocouple Range Fault");
+    if (fault & MAX31856_FAULT_CJHIGH)  Serial.println("Cold Junction High Fault");
+    if (fault & MAX31856_FAULT_CJLOW)   Serial.println("Cold Junction Low Fault");
+    if (fault & MAX31856_FAULT_TCHIGH)  Serial.println("Thermocouple High Fault");
+    if (fault & MAX31856_FAULT_TCLOW)   Serial.println("Thermocouple Low Fault");
+    if (fault & MAX31856_FAULT_OVUV)    Serial.println("Over/Under Voltage Fault");
+    if (fault & MAX31856_FAULT_OPEN)    Serial.println("Thermocouple Open Fault");
+  }
+
+  if ( TCTemp > 40 ) {
+    digitalWrite(heater, LOW);
+  } else {
+    digitalWrite(heater, HIGH);
+  }
+
+  delay(1000);
+}
